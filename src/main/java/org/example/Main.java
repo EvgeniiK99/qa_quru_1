d56397dfc8c801242e0533918a5df949bb3b6e04
Evgenii Klimashin <evgeniiklimashin99@gmail.com>
2023-05-26 01:33:56 +0300

    Fixe

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa.guru from Evgen!");
+        System.out.println("Hello qa.guru from Evgen!!!");
     }
 }
