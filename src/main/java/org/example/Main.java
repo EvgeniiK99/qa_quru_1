30f892fcae8fe78f5efaad3c4f4dbdcd94413c3e
Evgenii Klimashin <evgeniiklimashin99@gmail.com>
2023-05-26 12:35:29 +0300

    Add surName

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         String name = "Evgen";
-        System.out.println("Hello qa.guru from " + name);
-
+        String surName = "K.";
+        System.out.println("Hello qa.guru from " + name + " " + surName);
     }
 }
